1fbc86fc142c6aa9d5f90c288ac438df6b688922
Viorel Stirbu <viorel.stirbu@lpro.leverpointinc.com>
2011-05-06 15:32:02 +0300

    friendly orders listing output

--- orders.py
+++ orders.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
 
+import time
+
 from mtgox import MTGox
 import settings
 
 mtgox = MTGox(user=settings.MTGOX_USER, password=settings.MTGOX_PASSWORD)
 orders = mtgox.get_orders()
-print orders
+
+now = time.time()
+for order in orders:
+    order["type_text"] = {1: "sell", 2: "buy"}[order["type"]]
+    order["status_text"] = {1: "active", 
+                            2: "not enough funds"}[int(order["status"])]
+    order["ago"] = int((now - int(order["date"]))/60)
+    print ("%(oid)s %(type_text)s %(amount)s at %(price)s %(ago)s minutes ago, "
+           "%(status_text)s" % order)
 
